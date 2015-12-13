1fd94c290fabfbdca4799d0394381f9a957a497b
ship4sun <ship4sun@hanmail.net>
2015-12-13 20:42:58 +0900

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,8 @@
+hist(data$Global_active_power, 
+     main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)",
+     ylab="Frequency",
+     col="red")
+
+#Save through clicking, 'Plots', 'Export' and 'Save as an Image'.
+#You can adjust size at the window console. 
