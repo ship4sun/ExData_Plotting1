b71845ff81d170c1ea9662280050f5e7f06b00dd
ship4sun <ship4sun@hanmail.net>
2015-12-13 21:11:43 +0900

    Assignment#1

---
+++ plot2.R
@@ -0,0 +1,12 @@
+with(data, 
+     plot(Time, Global_active_power, 
+          xlab="", 
+          ylab="Global Active Power (kilowatts)", 
+          type="l"))
+
+# Thurseday = "∏Ò"
+# Friday = "±›"
+# Saterday = "≈‰"
+
+#Save through clicking, 'Plots', 'Export' and 'Save as an Image'.
+#You can adjust size at the window console. 
